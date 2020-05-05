d3f73004fdbe68b725ca7c29c7b68024393aba2b
Mert Altuntaş <59746147+mertfozzy@users.noreply.github.com>
2020-05-05 12:48:59 +0300

    Update Work.java

--- Work.java
+++ Work.java
@@ -1,3 +1,16 @@
+// Student Name: Mert Altuntas   | Student ID:1804010005 
+
+/* 
+ 
+Welcome to the Facial Emotion Detector V5 !! 
+ 
+If you want to see my progress step by step check it out our Github link!
+https://github.com/mertfozzy/Facial-Emotion-Detection
+  
+Enjoy.
+
+*/
+
 package calling.photo;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -11,106 +24,108 @@ import java.awt.SystemColor;
 
 
 
-public class Work extends JFrame{
-    JButton button ;
+public class Work extends JFrame {
+    JButton button;
     JLabel label;
     JLabel label2;
-    
-    public Work(){
-    super("FED - calling photo test ");
-    setTitle("Facial Emotion Detector v5");
-    getContentPane().setBackground(Color.DARK_GRAY);
-    
-    /* -----------------CHOOSING A PICTURE OPTION------------------- */
-  
-    button = new JButton("Browse a Photo !");
-    button.setForeground(Color.GREEN);
-    button.setBackground(Color.BLACK);
-    button.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
-    button.setBounds(733,464,214,60);
-    
-    label = new JLabel(); //displaying the picture
-    label.setHorizontalAlignment(SwingConstants.CENTER);
-    label.setBounds(145,0,961,422); 
-    
-    /* ---------------ENTERING A CURL COMMAND OPTION----------------- */
-    
-    JTextField text1 = new JTextField(50); //textbox
-    text1.setHorizontalAlignment(SwingConstants.CENTER);
-    text1.setText("Enter here..");
-    text1.setForeground(Color.WHITE);
-    text1.setBackground(Color.BLACK);
-    text1.setBounds(110, 400, 222, 52);
-    
-    JButton buton1 = new JButton("Browse a Curl !");
-    buton1.setBackground(Color.BLACK);
-    buton1.setForeground(Color.GREEN);
-    buton1.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
-    buton1.setBounds(110, 464, 222, 60);
-    
-    label2 = new JLabel(); //displaying result of curl command
-    label2.setHorizontalAlignment(SwingConstants.CENTER);
-    label2.setFont(new Font("Tahoma", Font.PLAIN, 20));
-    label2.setForeground(Color.WHITE);
-    label2.setBounds(132,28,798,352); 
 
-    /* ----------------------------------------------------- */
-    
-    getContentPane().add(button);
-    getContentPane().add(label);
-    getContentPane().add(text1);
-    getContentPane().add(buton1);
-    getContentPane().add(label2);
-    
-    buton1.addActionListener(new ActionListener() {
+    public Work() {
+        super("FED - calling photo test ");
+        setTitle("Facial Emotion Detector v5");
+        getContentPane().setBackground(Color.DARK_GRAY);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			
-			String url = text1.getText();
-			String curlCommand = "curl -u \"apikey:BmpffSkPjESnRKDYCoHVKaPQSSHCxrVQU8YS3-WOildA\" -F \"classifier_ids=DefaultCustomModel_2030462599\" \"https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/acfa1ca5-43c1-4b38-9a3d-9eff8477b8f8/v3/classify?url=" + url + "&version=2018-03-19\"";
-			String result = CurlUtil.executeCurlCommand(curlCommand.split(" "));
-			System.out.println("Curl command result : " + result);
-			label2.setText("<html><p style=\"width:900px\">"+result+"</p></html>"); //html code for displaying console result much better
-		}
-	});
-    
-    button.addActionListener(new ActionListener() {
+        /* -----------------CHOOSING A PICTURE OPTION------------------- */
 
-        public void actionPerformed(ActionEvent e) {
+        button = new JButton("Choose a Photo from library !");
+        button.setForeground(Color.RED);
+        button.setBackground(Color.BLACK);
+        button.setFont(new Font("Comic Sans MS", Font.BOLD, 17));
+        button.setBounds(613, 454, 334, 70);
+
+        label = new JLabel(); //displaying the picture
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setBounds(485, 11, 554, 412);
+        /* -------------------------------------------------------------- */
         
-          JFileChooser file = new JFileChooser();
-          file.setCurrentDirectory(new File(System.getProperty("user.home")));
-          
-          //filter the files
-          FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg","gif","png");
-          file.addChoosableFileFilter(filter);
-          int result = file.showSaveDialog(null);
-           
-          if(result == JFileChooser.APPROVE_OPTION){
-              File selectedFile = file.getSelectedFile();
-              String path = selectedFile.getAbsolutePath();
-              label.setIcon(ResizeImage(path));
-          }
-           
 
+        /* ---------------BROWSING A CURL COMMAND OPTION----------------- */
 
-          else if(result == JFileChooser.CANCEL_OPTION){
-              System.out.println("No File Select");
-          }
-        }
-    });
-    
-    getContentPane().setLayout(null);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLocationRelativeTo(null);
-    setSize(1065,585);
-    setVisible(true);
+        JTextField text1 = new JTextField(50); //textbox
+        text1.setHorizontalAlignment(SwingConstants.CENTER);
+        text1.setText("Enter here..");
+        text1.setForeground(Color.WHITE);
+        text1.setBackground(Color.BLACK);
+        text1.setBounds(110, 383, 334, 60);
+
+        JButton buton1 = new JButton("Browse a URL adress !");
+        buton1.setBackground(Color.BLACK);
+        buton1.setForeground(Color.GREEN);
+        buton1.setFont(new Font("Comic Sans MS", Font.BOLD, 17));
+        buton1.setBounds(110, 454, 334, 70);
+
+        label2 = new JLabel(); //displaying result of curl command
+        label2.setHorizontalAlignment(SwingConstants.CENTER);
+        label2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(132, 28, 798, 352);
+
+        /* -------------------------------------------------------------- */
+
+        getContentPane().add(button);
+        getContentPane().add(label);
+        getContentPane().add(text1);
+        getContentPane().add(buton1);
+        getContentPane().add(label2);
+
+        buton1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+            	
+       /* -------------------CONVERTING URL into CURL--------------------- */	
+
+                String url = text1.getText();
+                String curlCommand = "curl -u \"apikey:BmpffSkPjESnRKDYCoHVKaPQSSHCxrVQU8YS3-WOildA\" -F \"classifier_ids=DefaultCustomModel_2030462599\" \"https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/acfa1ca5-43c1-4b38-9a3d-9eff8477b8f8/v3/classify?url=" + url + "&version=2018-03-19\"";
+                String result = CurlUtil.executeCurlCommand(curlCommand.split(" "));
+                System.out.println("Curl command result : " + result);
+                label2.setText("<html><p style=\"width:900px\">" + result + "</p></html>"); //html code for displaying console result much better
+            }
+        });
+        /* -------------------------------------------------------------- */
+        
+        
+        button.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+
+                JFileChooser file = new JFileChooser();
+                file.setCurrentDirectory(new File(System.getProperty("user.home")));
+
+                //filter the files
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg", "gif", "png");
+                file.addChoosableFileFilter(filter);
+                int result = file.showSaveDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = file.getSelectedFile();
+                    String path = selectedFile.getAbsolutePath();
+                    label.setIcon(ResizeImage(path));
+                } else if (result == JFileChooser.CANCEL_OPTION) {
+                    System.out.println("No File Select");
+                }
+            }
+        });
+
+        getContentPane().setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(1065, 585);
+        setVisible(true);
     }
-     
-     
-    public ImageIcon ResizeImage(String ImagePath)
-    {
+
+
+    public ImageIcon ResizeImage(String ImagePath) {
         ImageIcon MyImage = new ImageIcon(ImagePath);
         Image img = MyImage.getImage();
         // Image newImg = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
@@ -118,8 +133,8 @@ public class Work extends JFrame{
         ImageIcon image = new ImageIcon(newImg);
         return image;
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         new Work();
     }
-   }
+}
