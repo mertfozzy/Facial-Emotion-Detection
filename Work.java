e4b3e23af4d5309f8aff15102cd0dbfe59e71481
Mert Altuntaş <59746147+mertfozzy@users.noreply.github.com>
2020-05-26 00:36:07 +0300

    Update Work.java

--- Work.java
+++ Work.java
@@ -13,14 +13,32 @@ Enjoy.
 
 package calling.photo;
 import java.awt.Image;
+import java.applet.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.SystemColor;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 
 
 
@@ -28,23 +46,24 @@ public class Work extends JFrame {
     JButton button;
     JLabel label;
     JLabel label2;
+    JLabel label3;
 
     public Work() {
         super("FED - calling photo test ");
-        setTitle("Facial Emotion Detector v5");
-        getContentPane().setBackground(Color.DARK_GRAY);
+        setTitle("Facial Emotion Detector v6");
+        getContentPane().setBackground(Color.BLACK);
 
         /* -----------------CHOOSING A PICTURE OPTION------------------- */
 
         button = new JButton("Choose a Photo from library !");
         button.setForeground(Color.RED);
-        button.setBackground(Color.BLACK);
+        button.setBackground(Color.DARK_GRAY);
         button.setFont(new Font("Comic Sans MS", Font.BOLD, 17));
-        button.setBounds(613, 454, 334, 70);
+        button.setBounds(611, 495, 334, 54);
 
         label = new JLabel(); //displaying the picture
         label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setBounds(485, 11, 554, 412);
+        label.setBounds(494, 11, 531, 473);
         /* -------------------------------------------------------------- */
         
 
@@ -54,20 +73,24 @@ public class Work extends JFrame {
         text1.setHorizontalAlignment(SwingConstants.CENTER);
         text1.setText("Enter here..");
         text1.setForeground(Color.WHITE);
-        text1.setBackground(Color.BLACK);
-        text1.setBounds(110, 383, 334, 60);
+        text1.setBackground(Color.DARK_GRAY);
+        text1.setBounds(110, 424, 334, 60);
 
-        JButton buton1 = new JButton("Browse a URL adress !");
-        buton1.setBackground(Color.BLACK);
+        JButton buton1 = new JButton("Browse a URL adress !"); // the button that takes actions
+        buton1.setBackground(Color.DARK_GRAY);
         buton1.setForeground(Color.GREEN);
         buton1.setFont(new Font("Comic Sans MS", Font.BOLD, 17));
-        buton1.setBounds(110, 454, 334, 70);
+        buton1.setBounds(110, 495, 334, 54);
 
         label2 = new JLabel(); //displaying result of curl command
         label2.setHorizontalAlignment(SwingConstants.CENTER);
         label2.setFont(new Font("Tahoma", Font.PLAIN, 20));
         label2.setForeground(Color.WHITE);
-        label2.setBounds(132, 28, 798, 352);
+        label2.setBounds(132, 28, 575, 352);
+        
+        label3 = new JLabel();
+        label3.setHorizontalAlignment(SwingConstants.CENTER);
+        label3.setBounds(484, 11, 541, 450);
 
         /* -------------------------------------------------------------- */
 
@@ -76,6 +99,8 @@ public class Work extends JFrame {
         getContentPane().add(text1);
         getContentPane().add(buton1);
         getContentPane().add(label2);
+        getContentPane().add(label3);
+        
 
         buton1.addActionListener(new ActionListener() {
 
@@ -90,8 +115,23 @@ public class Work extends JFrame {
                 String result = CurlUtil.executeCurlCommand(curlCommand.split(" "));
                 System.out.println("Curl command result : " + result);
                 label2.setText("<html><p style=\"width:900px\">" + result + "</p></html>"); //html code for displaying console result much better
+                URL url1;
+                try {
+                url1 = new URL(text1.getText());
+                BufferedImage image = ImageIO.read(url1);
+                label3.setIcon(new ImageIcon(image));
+                }
+                catch (MalformedURLException e1) {
+                e1.printStackTrace();
+                }
+                catch (IOException ex) {
+                ex.printStackTrace();
+                }
+                
+                
             }
         });
+        
         /* -------------------------------------------------------------- */
         
         
@@ -120,7 +160,7 @@ public class Work extends JFrame {
         getContentPane().setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setSize(1065, 585);
+        setSize(1065, 620);
         setVisible(true);
     }
 
@@ -128,7 +168,7 @@ public class Work extends JFrame {
     public ImageIcon ResizeImage(String ImagePath) {
         ImageIcon MyImage = new ImageIcon(ImagePath);
         Image img = MyImage.getImage();
-        // Image newImg = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
+      
         Image newImg = img.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
         ImageIcon image = new ImageIcon(newImg);
         return image;
